28a354eeac185ae03ce93ef4ced4ea80b702e230
guoxiangwen <gxw1990**>
2017-01-06 14:05:35 +0800

    提交代码

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,22 @@ let router = express.Router();
 let Mock = require('mockjs');
 
 
-const listsData = Mock.mock({
-  'list|1-10': [{
-    'id|+1': 1
-  }]
-})
+
 
 
 
 
 router.get('/list', function (req, res, next) {
+  const listsData = Mock.mock({
+    'list|0-10': [{
+      'id|+1': 1,
+      'name': '@name',
+      'email': '@email'
+    }],
+    'total|2-15': 5
+  })
   res.json(listsData);
+  res.end();
 });
 
 router.get('/pageList', function (req, res, next) {
